0a572e2985544aeecf0f2a515c1e2ac9ac7d9c33
dimstyle <smilehacking27@gmail.com>
2026-01-08 23:50:55 +0700

    bash update

--- bin/define_vm.sh
+++ bin/define_vm.sh
@@ -4,13 +4,25 @@
 
 shopt -s globstar
 
+OSVAR=""
+LISTOS=$(osinfo-query os --fields=short-id)
+echo "$LISTOS"
+while true ; do
+    read -r -p "chooose the os : " OSVAR
+    if printf "%s\n" $LISTOS | grep -Fqx $OSVAR; then
+        break
+    else
+        echo "OS variant not found"
+    fi
+done
+
 MEMORY=""
 while ! [[ $MEMORY =~ ^[1-9]+$ ]]; do
     read -p "memory (default 4096) : " MEMORY   
 done
 
 AMOUNT_CPU=""
-while ! [[ $AMOUNT_CPU=~ ^[1-9]+$ ]]; do
+while ! [[ $AMOUNT_CPU=~ '^[1-9]+$' ]]; do
     read -p "cpu (default 2) : " AMOUNT_CPU
 done
 
@@ -24,12 +36,23 @@ while ! [[ -f $DISK ]]; do
     read -p "path to disk file : " DISK
 done
 
+FORMAT=raw
+if qemu-img info $DISK | grep -q qcow2 ; then
+    $FORMAT=qcow2
+fi
 
-# virt-install -c qemu:///system \
-# --memory $MEMORY \
-# --cpu host-model \
-# --vcpus $AMOUNT_CPU \
-# --cdrom $CDROM \
+sudo rsync -avhLA /usr/share/OVMF/OVMF_VARS.fd .
+
+virt-install -c qemu:///system \
+--memory $MEMORY \
+--cpu host-model \
+--vcpus $AMOUNT_CPU \
+--cdrom $CDROM \
+--disk path=$DISK,format=$FORMAT,bus=virtio,serial=linux-disk \
+--boot loader=/usr/share/OVMF/OVMF_CODE.fd,loader.secure=no,loader.readonly=yes,loader_type=pflash,nvram=$(pwd)/OVMF_VARS \
+--network default \
+--graphics spice
+
 
 
 
